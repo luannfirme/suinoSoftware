cd39fc8a43c1fc39bb3d3bae1723079a73dd98ac
Víctor Quirino <victorsouzaq@gmail.com>
2024-03-10 15:39:47 -0300

    Serviço de controle de pesos e tela de cadastro e consulta de pesos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { HomeComponent } from './home/home.component';
 import { CadastroSuinoComponent } from './cadastro-suino/cadastro-suino.component';
 import { ListagemSuinosComponent } from './listagem-suinos/listagem-suinos.component';
 import { EditarSuinoComponent } from './editar-suino/editar-suino.component';
+import { CadastroPesoComponent } from './cadastro-peso/cadastro-peso.component';
+import { ControlePesoComponent } from './controle-peso/controle-peso.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'cadastrarSuino',component:CadastroSuinoComponent},
   {path: 'listarSuinos', component: ListagemSuinosComponent},
   {path: 'editarSuino/:id', component: EditarSuinoComponent},
+  {path: 'pesos/cadastrar', component: CadastroPesoComponent},
+  {path: 'pesos/consultar', component: ControlePesoComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import { ListagemSuinosComponent } from './listagem-suinos/listagem-suinos.compo
 import { ControlePesoComponent } from './controle-peso/controle-peso.component';
 import { CadastroPesoComponent } from './cadastro-peso/cadastro-peso.component';
 import { EditarSuinoComponent } from './editar-suino/editar-suino.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 
 @NgModule({
@@ -37,7 +39,9 @@ import { EditarSuinoComponent } from './editar-suino/editar-suino.component';
     MaterialModule,
     BrowserAnimationsModule,
     NoopAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"feb-p015","appId":"1:526851697276:web:c4ececcb788d48337cf7f9","databaseURL":"https://feb-p015-default-rtdb.firebaseio.com","storageBucket":"feb-p015.appspot.com","apiKey":"AIzaSyDbAsoB7n_PDZZY6Zr0J1npeUb3ktHXw9E","authDomain":"feb-p015.firebaseapp.com","messagingSenderId":"526851697276","measurementId":"G-JWM3GN0BVH"})),
+    provideAuth(() => getAuth())
   ],
   providers: [
     provideAnimationsAsync()
