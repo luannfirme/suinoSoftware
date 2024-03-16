2f1e6c1fa91986434ca9fcc44ba1024fb133e931
Víctor Quirino <victorsouzaq@gmail.com>
2024-03-16 20:04:19 -0300

    Campos de data e seleção de brincos

--- src/app/cadastro-sessao/cadastro-sessao.component.ts
+++ src/app/cadastro-sessao/cadastro-sessao.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pig } from '../models/pig.model';
+import { DatabaseService } from '../services/database/database.service';
 
 @Component({
   selector: 'app-cadastro-sessao',
   templateUrl: './cadastro-sessao.component.html',
   styleUrl: './cadastro-sessao.component.css'
 })
-export class CadastroSessaoComponent {
+export class CadastroSessaoComponent implements OnInit {
+
+  pigs: Pig[] = [];
+
+  constructor(private dbService: DatabaseService) { }
+
+  ngOnInit(): void {
+    this.dbService.getPigs().subscribe(response => {
+      this.pigs = response;
+    })
+  }
 
 }
